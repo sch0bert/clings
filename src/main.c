3b29768e71aa27b84332082a33ee30d1ff01098e
sch0bert <pascal.schobert@gmail.com>
2023-11-03 15:41:13 +0100

    Fix to be able to read sensor XYZ and temp data

--- src/main.c
+++ src/main.c
@@ -7,33 +7,72 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/init.h>
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/logging/log.h>
-#include "lis2dtw12.h"
+#include <zephyr/drivers/sensor.h>
 
-#define DT_DRV_COMPAT st_lis2dtw12
 #define SLEEP_TIME_MS   1000
 
 LOG_MODULE_REGISTER(Main, CONFIG_SENSOR_LOG_LEVEL);
 
-void main(void)
+static void fetch_and_display(const struct device *sensor)
 {
-	LOG_INF("LIS2DTW12 accelerometer sensor");
-	if (!device_is_ready(dev_i2c.bus)) {
-		LOG_ERR("I2C bus %s is not ready!\n\r",dev_i2c.bus->name);
-		return;
+	static unsigned int count;
+	struct sensor_value accel[3];
+	struct sensor_value temperature;
+	const char *overrun = "";
+	int rc = sensor_sample_fetch(sensor);
+
+	++count;
+	if (rc == -EBADMSG) {
+		/* Sample overrun.  Ignore in polled mode. */
+		if (IS_ENABLED(CONFIG_LIS2DW12_TRIGGER)) {
+			overrun = "[OVERRUN] ";
+		}
+		rc = 0;
+	}
+	if (rc == 0) {
+		rc = sensor_channel_get(sensor,
+					SENSOR_CHAN_ACCEL_XYZ,
+					accel);
+	}
+	if (rc < 0) {
+		LOG_ERR("ERROR: Update failed: %d", rc);
+	} else {
+		LOG_INF("#%u @ %u ms: %sx %f , y %f , z %f",
+		       count, k_uptime_get_32(), overrun,
+		       sensor_value_to_double(&accel[0]),
+		       sensor_value_to_double(&accel[1]),
+		       sensor_value_to_double(&accel[2]));
 	}
 
-	int ret = lis2dtw12_init(dev_i2c.bus);
-	if (ret < 0) {
-		LOG_ERR("Failed to initialize i2c interface for LIS2DTW12, error: %d\n\r", ret);
-		return;
+	if (rc == 0) {
+		rc = sensor_channel_get(sensor, SENSOR_CHAN_AMBIENT_TEMP, &temperature);
+		if (rc < 0) {
+			LOG_ERR("ERROR: Unable to read temperature:%d", rc);
+		} else {
+			LOG_INF(", t %f", sensor_value_to_double(&temperature));
+		}
+	}	
+}
+
+int main(void)
+{
+	const struct device *const sensor = DEVICE_DT_GET_ANY(st_lis2dw12);
+
+	if (sensor == NULL) {
+		LOG_ERR("No device found");
+		return 0;
+	}
+	if (!device_is_ready(sensor)) {
+		LOG_ERR("Device %s is not ready", sensor->name);
+		return 0;
 	}
 
-	while (1) {
-
+	while (true) {
+		fetch_and_display(sensor);
 		k_msleep(SLEEP_TIME_MS);
 	}
 }
+
